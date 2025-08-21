6a24c4f31ff9b29667b15a652fbffd2123f924ba
david1zhong <127060667+david1zhong@users.noreply.github.com>
2025-08-21 17:24:26 -0400

    Update model_CL3_LEARN.py

--- models/model_CL3_LEARN.py
+++ models/model_CL3_LEARN.py
@@ -18,7 +18,7 @@ load_dotenv()
 USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 HOST = os.getenv("DB_HOST")
-PORT = 6543
+PORT = os.getenv("DB_PORT", "6543")
 DBNAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
