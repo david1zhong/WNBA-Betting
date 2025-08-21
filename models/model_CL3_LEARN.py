b6b52b06d0a88370c2fc92ca473501affed45f6b
david1zhong <127060667+david1zhong@users.noreply.github.com>
2025-08-21 17:11:43 -0400

    Update model_CL3_LEARN.py

--- models/model_CL3_LEARN.py
+++ models/model_CL3_LEARN.py
@@ -17,9 +17,8 @@ USER = os.getenv("DB_USER"),
 PASSWORD = os.getenv("DB_PASSWORD"),
 HOST = os.getenv("DB_HOST"),
 DBNAME = os.getenv("DB_NAME"),
-PORT=6543
 
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:6543/{DBNAME}?sslmode=require"
 engine = create_engine(DATABASE_URL)
 
 
